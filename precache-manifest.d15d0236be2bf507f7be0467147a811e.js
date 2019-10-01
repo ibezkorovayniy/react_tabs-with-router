00e9ae17fcf01a5a2d18074fb1027af542636fbd
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-10-02 01:52:05 +0300

    Updates

---
+++ precache-manifest.d15d0236be2bf507f7be0467147a811e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72ed0670712ef4dcf3af4b574be2276",
+    "url": "/.github.io/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "27462da0c665dffc8bdd",
+    "url": "/.github.io/react_tabs-with-router/static/css/main.892a3e82.chunk.css"
+  },
+  {
+    "revision": "d0cea113b3283f23d6ba",
+    "url": "/.github.io/react_tabs-with-router/static/js/2.d5446fe0.chunk.js"
+  },
+  {
+    "revision": "27462da0c665dffc8bdd",
+    "url": "/.github.io/react_tabs-with-router/static/js/main.5c1c901a.chunk.js"
+  },
+  {
+    "revision": "03285b58b9b053749430",
+    "url": "/.github.io/react_tabs-with-router/static/js/runtime~main.986f02f5.js"
+  }
+]);
