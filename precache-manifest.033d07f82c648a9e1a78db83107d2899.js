0fc68cd7f6d556c2a456ae187372d873ad0063ac
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-10-02 01:58:24 +0300

    Updates

--- precache-manifest.033d07f82c648a9e1a78db83107d2899.js
+++ precache-manifest.033d07f82c648a9e1a78db83107d2899.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b72ed0670712ef4dcf3af4b574be2276",
+    "revision": "b8d9d6b8000401ca348b5337905c6eee",
     "url": "/.github.io/react_tabs-with-router/index.html"
   },
   {
-    "revision": "27462da0c665dffc8bdd",
+    "revision": "f59aa4b1d19733e18195",
     "url": "/.github.io/react_tabs-with-router/static/css/main.892a3e82.chunk.css"
   },
   {
-    "revision": "d0cea113b3283f23d6ba",
-    "url": "/.github.io/react_tabs-with-router/static/js/2.d5446fe0.chunk.js"
+    "revision": "ca40d0d52fb465f7bed7",
+    "url": "/.github.io/react_tabs-with-router/static/js/2.30ad687b.chunk.js"
   },
   {
-    "revision": "27462da0c665dffc8bdd",
-    "url": "/.github.io/react_tabs-with-router/static/js/main.5c1c901a.chunk.js"
+    "revision": "f59aa4b1d19733e18195",
+    "url": "/.github.io/react_tabs-with-router/static/js/main.594bd30f.chunk.js"
   },
   {
     "revision": "03285b58b9b053749430",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/.github.io/react_tabs-with-router/precache-manifest.d15d0236be2bf507f7be0467147a811e.js"
+  "/.github.io/react_tabs-with-router/precache-manifest.033d07f82c648a9e1a78db83107d2899.js"
 );
 
 self.addEventListener('message', (event) => {
